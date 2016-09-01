c339b783a48c7d20ee552c4fdfbf979f5a0e1ae5
danfercf1 <danfercf@gmail.com>
2016-09-01 00:09:15 -0400

    First commit

---
+++ src/danfercf/persistentHandler/PersistentDataHandler.php
@@ -0,0 +1,32 @@
+<?php
+namespace danfercf\persistentHandler;
+use \Facebook\PersistentData\PersistentDataInterface;
+use \Yii\Web\Session;
+
+class PersistentDataHandler implements PersistentDataInterface
+{
+    /**
+     * @var string Prefix to use for session variables.
+     */
+    protected $sessionPrefix = 'FBRLH_';
+
+    /**
+     * @inheritdoc
+     */
+    public function get($key)
+    {
+        $session = new Session;
+        $session->open();
+        return  $session[$this->sessionPrefix . $key];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function set($key, $value)
+    {
+        $session = new Session;
+        $session->open();
+        $session[$this->sessionPrefix . $key] = $value;
+    }
+}
